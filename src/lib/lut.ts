a4837252ae54195a42acac2defb25bf74e4b0e29
Martin Tomasi <martin.tomasi@gmail.com>
2022-09-26 23:45:11 +0200

    some parsing stuff

--- src/lib/lut.ts
+++ src/lib/lut.ts
@@ -6,10 +6,66 @@ const DOMAIN_MIN_REGEX = /DOMAIN_MIN ([.0-9]+)\s+([.0-9]+)/
 const DOMAIN_MAX_REGEX = /DOMAIN_MAX ([.0-9]+)\s+([.0-9]+)/
 const SIZE_REGEX = /LUT_3D_SIZE ([0-9]+)/
 
-export interface Rgb {
-  r: number
-  g: number
-  b: number
+type LineType = "color" | "domainMin" | "domainMax" | "size" | "title"
+
+const REGEXES: {regex: RegExp; type: LineType}[] = [
+  {regex: RGB_REGEX, type: "color"},
+  {regex: TITLE_REGEX, type: "title"},
+  {regex: DOMAIN_MAX_REGEX, type: "domainMax"},
+  {regex: DOMAIN_MIN_REGEX, type: "domainMin"},
+  {regex: SIZE_REGEX, type: "size"},
+]
+
+export type Rgb = [number, number, number]
+
+type Line =
+  | {
+      type: "color"
+      value: Rgb
+    }
+  | {
+      type: "domainMin"
+      value: Rgb
+    }
+  | {
+      type: "domainMax"
+      value: Rgb
+    }
+  | {
+      type: "size"
+      value: number
+    }
+  | {
+      type: "title"
+      value: string
+    }
+
+function matchRegexes(line: string): Line | undefined {
+  for (const {regex, type} of REGEXES) {
+    const match = regex.exec(line)
+    if (match) {
+      switch (type) {
+        case "color":
+        case "domainMin":
+        case "domainMax": {
+          const r = parseFloat(match[1])
+          const g = parseFloat(match[2])
+          const b = parseFloat(match[3])
+          return {type, value: [r, g, b]}
+        }
+
+        case "size": {
+          return {type, value: parseInt(match[1])}
+        }
+
+        case "title": {
+          return {type, value: match[1].trim()}
+        }
+      }
+    }
+  }
+
+  return undefined
 }
 
 export class CubeLut {
@@ -30,13 +86,13 @@ export function parseCubeLut(input: string) {
     if (line.startsWith("#")) {
       return
     }
-
-    const match = RGB_REGEX.exec(line)
-    if (match?.length === 4) {
-      const r = parseFloat(match[1])
-      const g = parseFloat(match[2])
-      const b = parseFloat(match[3])
-      data.push({r, g, b})
+    const parsedLine = matchRegexes(line)
+    if (parsedLine) {
+      switch (parsedLine.type) {
+        case "color":
+          data.push(parsedLine.value)
+          break
+      }
     }
   })
 
